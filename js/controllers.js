e5798540c9e44ea0afeed4535bf77d0d95bb25e6
tengyao <anewhuahua@gmail.com>
2016-01-10 00:39:46 +0800

    01.09.1

--- js/controllers.js
+++ js/controllers.js
@@ -1285,11 +1285,52 @@ angular.module('starter.controllers', [])
   $scope.doRefresh = function() {
     setTimeout(function(){
       console.log('refresh');
+      /*
       Main.getProducts($scope.data.category, function(data){
       }, function(status){}, function(){
         $scope.$broadcast('scroll.refreshComplete');
         $scope.data.more=true;
       });
+      */
+
+      if($scope.data.key == 1) {  //近1周
+        param = {sortby: 'weekRate', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 2) { //近1月
+        param = {sortby: 'monRate1', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 3) { //近3月
+        param = {sortby: 'monRate3', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 4) { // 今年来
+        param = {sortby: 'yearRate0', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 5) { // 近1年
+        param = {sortby: 'yearRate1', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 6) { // 近3年
+        param = {sortby: 'yearRate3', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 7) { // 整体收益
+        param = {sortby: 'sumRate', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else if ($scope.data.key == 8) { // 单位净值
+        param = {sortby: 'Nav', sort: 'desc', subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      } else {
+        param = {subtype: $scope.data.currentSubType};
+        Main.sortProducts($scope.data.category, param,
+          function(data){}, function(status){}, function(){});
+      }
+
     }, 1000);
   };
 
